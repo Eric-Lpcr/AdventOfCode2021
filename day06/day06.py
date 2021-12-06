8e3e25b911813b73266a51a5457433f13fd363ed
Eric-Lpcr <lepicier.eric@gmail.com>
2021-12-06 23:19:37 +0100

    Day 6 solution

--- day06/day06.py
+++ day06/day06.py
@@ -1,7 +1,7 @@
 
-def compute_lantern_fish_population(initial, days):
+def compute_lantern_fish_population(fishes, days):
+    fish_count = [fishes.count(days_left) for days_left in range(9)]  # need to manage 0 to 8 days left population
     new_cycle_start = 6
-    fish_count = list(initial)
     for _ in range(days):
         youngsters = times_up = fish_count.pop(0)
         fish_count[new_cycle_start] += times_up
@@ -14,11 +14,10 @@ def main(filename):
     with open(filename) as f:
         fishes = [int(s) for s in f.readline().split(',')]
 
-    fish_count = [fishes.count(days_left) for days_left in range(9)]  # need to manage 0 to 8 days left population
     for days in [18, 80]:
-        print(f'Part 1: after {days} days, count of fishes is {compute_lantern_fish_population(fish_count, days)}')
+        print(f'Part 1: after {days} days, count of fishes is {compute_lantern_fish_population(fishes, days)}')
     days = 256
-    print(f'Part 2: after {days} days, count of fishes is {compute_lantern_fish_population(fish_count, days)}')
+    print(f'Part 2: after {days} days, count of fishes is {compute_lantern_fish_population(fishes, days)}')
 
 
 if __name__ == '__main__':
