282f93b85026e40cb82b1fa4e8015da920d03a85
Eric-Lpcr <lepicier.eric@gmail.com>
2021-12-06 23:13:52 +0100

    Day 6 solution

--- day06/day06.py
+++ day06/day06.py
@@ -1,12 +1,12 @@
 
 def compute_lantern_fish_population(initial, days):
     new_cycle_start = 6
-    fish_states = list(initial)
+    fish_count = list(initial)
     for _ in range(days):
-        youngsters = times_up = fish_states.pop(0)
-        fish_states[new_cycle_start] += times_up
-        fish_states.append(youngsters)
-    return sum(fish_states)
+        youngsters = times_up = fish_count.pop(0)
+        fish_count[new_cycle_start] += times_up
+        fish_count.append(youngsters)
+    return sum(fish_count)
 
 
 def main(filename):
@@ -14,11 +14,11 @@ def main(filename):
     with open(filename) as f:
         fishes = [int(s) for s in f.readline().split(',')]
 
-    fish_states = [fishes.count(days_left) for days_left in range(9)]  # need to manage 0 to 8 population
+    fish_count = [fishes.count(days_left) for days_left in range(9)]  # need to manage 0 to 8 days left population
     for days in [18, 80]:
-        print(f'Part 1: after {days} days, count of fishes is {compute_lantern_fish_population(fish_states, days)}')
+        print(f'Part 1: after {days} days, count of fishes is {compute_lantern_fish_population(fish_count, days)}')
     days = 256
-    print(f'Part 2: after {days} days, count of fishes is {compute_lantern_fish_population(fish_states, days)}')
+    print(f'Part 2: after {days} days, count of fishes is {compute_lantern_fish_population(fish_count, days)}')
 
 
 if __name__ == '__main__':
