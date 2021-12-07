19f3fc7fab83bd2526f632556a6169136853bc26
Eric-Lpcr <lepicier.eric@gmail.com>
2021-12-07 18:36:08 +0100

    sliding_window from itertools recipes

--- day01/wise.py
+++ day01/wise.py
@@ -1,3 +1,4 @@
+import collections
 from itertools import tee, islice
 
 
@@ -23,6 +24,18 @@ def n_wise2(iterable, size):
         yield tuple(islice(iterable, index, index + size))
 
 
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
 def map_wise(fn, iterable, size):
     return (fn(item) for item in n_wise2(iterable, size))
 
@@ -32,6 +45,7 @@ if __name__ == '__main__':
     print(list(n_wise('ABCDEFG', 3)))
     print(list(n_wise('ABCDEFG', 4)))
     print(list(n_wise2('ABCDEFG', 3)))
+    print(list(sliding_window('ABCDEFG', 4)))
 
     print(list(map_wise(lambda t: ''.join(t), 'ABCDEFG', 3)))
     print(list(map_wise(sum, range(10), 4)))
