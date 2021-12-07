a44f7b1d26dfe05ba26ea29a6ff2e1d04255523e
Eric-Lpcr <lepicier.eric@gmail.com>
2021-12-07 20:47:29 +0100

    Day 7 solution

---
+++ day07/README.md
@@ -0,0 +1,32 @@
+# [Day 7: The Treachery of Whales](https://adventofcode.com/2021/day/7)
+
+I wouldn't solve this one with brute force, but my mathematical background was not strong enough.
+I googled for a minimization of deltas from data to a value, and found some articles explaining the difference
+between mean and median.
+
+In french sorry:
+- [Et si on s’intéressait à la moyenne des écarts ?](https://www.apmep.fr/IMG/pdf/AAA14048.pdf)
+- [Calcul de la médiane](https://www150.statcan.gc.ca/n1/edu/power-pouvoir/ch11/median-mediane/5214872-fra.htm)
+
+The first one states "The median minimizes the deltas"... Here we are, part 1 will be solved by computing the median 
+value. Next challenge was not to sort the whole data, but to work with a cumulative frequency histogram, looking for the
+first value reaching half the total number of values.
+
+I secretly hoped that part 2 would work as well, changing only the fuel consumption method. Ooops, reading carefully the
+challenge showed that the optimal value was also changing and should be computed differently. I let the problem mature 
+for a few hours...
+
+Part 2 fuel consumption is a classic running sum `1 + 2 + 3 + 4 + ... + k` which equals to `k * (k + 1) / 2`. k * k is a 
+square (wow!) and I read in the papers that the mean mas minimizing the delta squares... Here we are for part 2.
+
+But unlike which is a number taken in the data, mean is a computation and gives a decimal result in our case. So I tried
+with the two surrounding integers and chose the one which was giving the lowest total consumption.
+
+I'm not fully satisfied with part 2 solution because it's not strongly designed with mathematics, but resides in a 
+lucky feeling attempt ;-)
+
+By the way, no Python trick in this solution except classic itertools, but a real maths challenge.
+
+Loved this one later:
+[On the Unreasonable Efficacy of the Mean in Minimizing the Fuel Expenditure of Crab 
+Submarines](https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/)
---
+++ day07/day07.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+from itertools import accumulate
+
+
+def histogram(data):
+    counts = defaultdict(int)
+    for d in data:
+        counts[d] += 1
+    return counts
+
+
+def median(data):
+    counts = histogram(data)
+    distinct_values = sorted(counts.keys())
+    frequencies = [counts[d] for d in distinct_values]
+    cumulated_frequencies = accumulate(frequencies)
+    total_count = sum(frequencies)
+    cumulated_relative_frequencies = [f / total_count for f in cumulated_frequencies]
+    median_index = next(i for i, frequency in enumerate(cumulated_relative_frequencies) if frequency > 0.5)
+    median_value = distinct_values[median_index]
+    return median_value, counts
+
+
+def mean(data):
+    counts = histogram(data)
+    total_count = sum(counts.values())
+    mean_value = sum(d * c for d, c in counts.items()) / total_count
+    return mean_value, counts
+
+
+def compute_minimal_fuel1(crab_positions):
+    optimal_crab_position, counts = median(crab_positions)
+    fuel = sum((position - optimal_crab_position) * count for position, count in counts.items())
+    return fuel, optimal_crab_position
+
+
+def sum_of_k(k):
+    """Sum of the k first integers starting at 1"""
+    k = abs(int(k))  # k must be a positive integer
+    return int(k * (k + 1) / 2)
+
+
+def compute_minimal_fuel2(crab_positions):
+    optimal_crab_position, counts = mean(crab_positions)
+
+    # mean() is a decimal, let's try with previous and next int values
+    optimal_crab_position1 = int(optimal_crab_position)
+    fuel1 = sum(sum_of_k(position - optimal_crab_position1) * count for position, count in counts.items())
+
+    optimal_crab_position2 = int(round(optimal_crab_position, 0))
+    fuel2 = sum(sum_of_k(position - optimal_crab_position2) * count for position, count in counts.items())
+
+    if fuel1 < fuel2:
+        return fuel1, optimal_crab_position1
+    else:
+        return fuel2, optimal_crab_position2
+
+
+def main(filename):
+    print(f'--------- {filename}')
+    with open(filename) as f:
+        crab_positions = [int(s) for s in f.readline().split(',')]
+
+    fuel, position = compute_minimal_fuel1(crab_positions)
+    print(f'Part 1: optimal crabs position is {position}, minimal fuel consumption is {fuel}')
+
+    fuel, position = compute_minimal_fuel2(crab_positions)
+    print(f'Part 2: optimal crabs position is {position}, minimal fuel consumption is {fuel}')
+
+
+if __name__ == '__main__':
+    main('test.txt')
+    main('input.txt')
